36095b89381f4c88636a2f5368476c8ff4c8e110
nakgamers <39780289+nakgamers@users.noreply.github.com>
2019-05-27 03:04:18 +0700

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,45 @@
+clear
+sleep 1 
+echo  "======================"
+echo "    [MPS_YT TOOLS]     "
+echo " Coded by : Nandar SGB "
+echo "======================="
+date
+sleep 1 
+	echo "Silahkan pilih Setup Modul, jika pertama kali Run"
+	echo " 1.Run Tools "
+	echo " 2.setup modul "
+	echo " 3.info Tools (guide) " 
+sleep 1
+echo ""
+echo "Nomer Brp bos?"
+read no
+if [ $no = 1 ]
+then
+clear
+bash down.sh
+fi
+
+if [ $no = 2 ] 
+then
+clear
+bash modul.sh
+fi
+
+if [ $no = 3 ]
+then
+clear
+echo " =======================[MPS-YT TOOLS]=============================  "
+sleep 1
+echo " Fungsi : Mencari judul lagu,Streaming/Donwload lagu/video dari pencarian "
+sleep 1
+echo " how to use : Run Tool -> searh lagu menggunakn / (slash)  "
+sleep 1
+echo " example: /alan walker (akan mencari keyword alan walker) "
+sleep 1
+echo " key fungsi : bila result searh sudah muncul, ketik Nomer result untuk streaming "
+sleep 1
+echo " ketik d (nomer result) untuk donwload. example: d 1/2/dst "
+sleep 1
+echo "===========================[SGBTeam]============================== "
+fi
---
+++ modul.sh
@@ -0,0 +1,27 @@
+clear
+sleep 1
+echo "-------------------------"
+echo " [X] MPS modul Setup [x] "
+echo "-------------------------"
+sleep 1
+echo ""
+echo "[>] mulai setup? (y/n)"
+read yn 
+sleep 1
+if [ $yn = y ]
+then 
+echo "menyiapkan command.."
+pkg install -y python ffmpeg mpv
+echo "memulai setup kedua"
+sleep 1
+pip install youtube-dl
+pip install mps_youtube
+clear
+echo "setup selesai, memulai ulang tools..."
+bash index.sh
+fi
+
+if [ $yn = n ]
+then 
+echo "Setup Modul Berhenti"
+fi
